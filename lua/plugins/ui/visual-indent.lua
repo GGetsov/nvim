539ea4f208d869e1f2a890c936d05c6f7a589c1d
G.Getsov <G.Getsov.Dev@gmail.com>
2023-09-15 16:56:57 +0300

    Visual indent

--- lua/plugins/ui/visual-indent.lua
+++ lua/plugins/ui/visual-indent.lua
@@ -1,7 +1,7 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	lazy = true,
-	-- event = "BufRead",
+	event = "BufRead",
 	config = function()
 		CustomHl = vim.api.nvim_get_hl_by_name("Include", true)
 		vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = CustomHl.foreground })
