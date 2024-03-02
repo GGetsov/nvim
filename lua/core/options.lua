8f09a3afe716e34cf704d8d6cb00193ac209b94a
GGetsov <g.getsov.dev@gmail.com>
2024-03-02 23:52:35 +0200

    2 spaces default

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,24 +2,24 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- 4 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = true -- disable line wrapping
-opt.showbreak = ">> " -- add ">>" before every line that wraps
+opt.wrap = true                             -- disable line wrapping
+opt.showbreak = ">> "                       -- add ">>" before every line that wraps
 opt.cpoptions = opt.cpoptions._value .. "n" -- show the ">>" between the numbers
-opt.breakindent = true -- wrapped lines are indented too
-opt.breakindentopt = "sbr" -- keeps the ">>" unindented
+opt.breakindent = true                      -- wrapped lines are indented too
+opt.breakindentopt = "sbr"                  -- keeps the ">>" unindented
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -30,7 +30,7 @@ opt.cursorline = true -- highlight the current cursor line
 -- (have to use true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -39,8 +39,8 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true     -- split vertical window to the right
+opt.splitbelow = true     -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
 
@@ -53,11 +53,11 @@ opt.scrolloff = 5
 
 --shell setup
 if ON_WINDOWS then
-	opt.shell = "powershell"
-	opt.shellcmdflag =
-		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-	opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-	opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-	opt.shellquote = ""
-	opt.shellxquote = ""
+  opt.shell = "powershell"
+  opt.shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  opt.shellquote = ""
+  opt.shellxquote = ""
 end
